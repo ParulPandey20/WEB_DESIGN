883846359a2f0ffa50dc607fda66efbcaabf6c79
ParulPandey20 <ParulPandey@Paruls-MacBook-Pro.local>
2017-04-03 01:31:53 -0400

    Wollaston Website

---
+++ WebDesign_Final/Deli/about.php
@@ -0,0 +1,147 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Wollaston's</title>
+
+    <!-- Bootstrap Core CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom CSS -->
+    <link href="css/business-casual.css" rel="stylesheet">
+
+    <!-- Fonts -->
+    <link href="http://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800" rel="stylesheet" type="text/css">
+    <link href="http://fonts.googleapis.com/css?family=Josefin+Slab:100,300,400,600,700,100italic,300italic,400italic,600italic,700italic" rel="stylesheet" type="text/css">
+
+</head>
+
+<body>
+
+    <div class="brand">Wollaston's</div>
+    <div class="address-bar">"Your neighbourhood grocer" </div>
+
+    <!-- Navigation -->
+    <nav class="navbar-default" role="navigation">
+        <div class="container">
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <!-- navbar-brand is hidden on larger screens, but visible when the menu is collapsed -->
+                <a class="navbar-brand" href="index.html">Wollaston's</a>
+            </div>
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <ul class="nav navbar-nav">
+                    <li>
+                        <a href="../index.html">Home</a>
+                    </li>
+                    <li>
+                        <a href="about.php">AboutUs</a>
+                    </li>
+                    <li>
+                        <a href="../Bakery/bakery.html">Bakery</a>
+                    </li>
+                    <li>
+                        <a href="deli.php">Deli</a>
+                    </li>
+                    <li>
+                        <a href="../Grocery/grocery.html">Groceries</a>
+                    </li>
+                    <li>
+                        <a href="../Grocery/contact.php">Contact Us</a>
+                    </li>
+                </ul>
+            </div>
+            <!-- /.navbar-collapse -->
+        </div>
+        <!-- /.container -->
+    </nav>
+    <br/>
+	<br/>
+	
+     <div class="container">
+
+        <div class="row">
+            <div class="box">
+                <div class="col-lg-12">
+                    <hr>
+                    <h2 class="intro-text text-center">About
+                        <strong>Wollaston's</strong>
+                    </h2>
+                    <hr>
+                </div>
+                <div class="col-md-6">
+                    <img class="img-responsive img-rounded" src="img/bg1.jpg" alt="">
+                </div>
+                <div class="col-md-6">
+                    <p>We started out as a small corner store in Wollaston, MA in the neighbourhood of Boston long before we shifted to Northeastern in 1980.</p>
+                    <p>We have two stores in Northeastern now, one in Marino fitness center and the other in Northeasterns west village campus on parker street.</p>
+                    <p>We are proud to be part of Northeastern, and we are a major contributor towards daily needs of our students.</p>
+                </div>
+                <div class="clearfix"></div>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="box">
+                <div class="col-lg-12">
+                    <hr>
+                    <h2 class="intro-text text-center">Our Store
+                        <strong>Manager</strong>
+                    </h2>
+                    <hr>
+                </div>
+                    <div class="col-sm-6 text-center">
+                        <img class="img-responsive" src="img/gallery/marianne.jpg" alt="" height="300px" length="400px">
+                        <h4>Maryanne Crawford</h4>
+                    </div>
+                    <div class="col-sm-6 text-center">
+                            <p style="padding:300px 0 0 0;">"I have been working at wollastons for 20 years now. We are proud to say that we have become a very important arm of Northeastern. We serve more than 300 students on a regular day by catering to their needs."</p>
+                    </div>
+                </div>
+                <div class="clearfix"></div>
+            </div>
+    </div>
+    
+
+    <footer>
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12 text-center">
+                    <p>Copyright &copy; Your Website 2014</p>
+                </div>
+            </div>
+        </div>
+    </footer>
+
+    <!-- jQuery -->
+    <script src="js/jquery.js"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="js/bootstrap.min.js"></script>
+
+    <!-- Script to Activate the Carousel -->
+    <script>
+    $('.carousel').carousel({
+        interval: 5000 //changes the speed
+    })
+    </script>
+
+</body>
+
+</html>
+
+
